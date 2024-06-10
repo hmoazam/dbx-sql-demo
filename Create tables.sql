68975d0b12037238c74dc5530cd668b04d2fc147
hmoazam <hmoazam>
2024-06-10 23:54:53 +0000

    Created sql notebooks

---
+++ Create tables.sql
@@ -0,0 +1,41 @@
+-- Databricks notebook source
+--STORE DIMENSION
+CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.dim_store(
+  store_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  store_name STRING,
+  address STRING
+);
+
+
+
+-- COMMAND ----------
+
+--PRODUCT DIMENSION
+CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.dim_product(
+  product_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  sku STRING,
+  description STRING,
+  category STRING
+);
+
+-- COMMAND ----------
+
+--CUSTOMER DIMENSION
+CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.dim_customer(
+  customer_id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 0 INCREMENT BY 10) PRIMARY KEY,
+  customer_name STRING,
+  customer_profile STRING,
+  address STRING
+);
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.fact_sales(
+  sales_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  product_id BIGINT NOT NULL CONSTRAINT dim_product_fk FOREIGN KEY REFERENCES dim_product,
+  store_id BIGINT NOT NULL CONSTRAINT dim_store_fk FOREIGN KEY REFERENCES dim_store,
+  customer_id BIGINT NOT NULL CONSTRAINT dim_customer_fk FOREIGN KEY REFERENCES dim_customer,
+  price_sold DOUBLE,
+  units_sold INT,
+  dollar_cost DOUBLE
+);
---
+++ Populate tables.sql
@@ -0,0 +1,32 @@
+-- Databricks notebook source
+INSERT INTO
+  uc_demos_hanna_moazam.default.dim_store (store_name, address)
+VALUES
+  ('City Store', '1 Main Rd, Whoville');
+
+-- COMMAND ----------
+
+INSERT INTO
+  uc_demos_hanna_moazam.default.dim_product (sku, description, category)
+VALUES
+  ('1000001', 'High Tops', 'Ladies Shoes'),
+  ('7000003', 'Printed T', 'Ladies Fashion Tops');
+
+-- COMMAND ----------
+
+INSERT INTO
+  uc_demos_hanna_moazam.default.dim_customer (customer_name, customer_profile, address)
+VALUES
+  ('Al', 'Al profile', 'Databricks - Queensland Australia'),
+  ('Quentin', 'REDACTED_PROFILE', 'Databricks - Paris France');
+
+-- COMMAND ----------
+
+INSERT INTO
+  uc_demos_hanna_moazam.default.fact_sales (product_id, store_id, customer_id, price_sold, units_sold, dollar_cost)
+VALUES
+  (1, 1, 0, 100.99, 2, 2.99),
+  (2, 1, 0, 10.99, 2, 2.99),
+  (1, 1, 0, 100.99, 2, 2.99),
+  (1, 1, 10, 100.99, 2, 2.99),
+  (2, 1, 10, 10.99, 2, 2.99);

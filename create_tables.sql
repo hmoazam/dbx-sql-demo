f85b6aa0830d079a59c8983e2c8c1019b0523fba
hmoazam <hmoazam>
2024-06-11 00:33:28 +0000

    fix foreign key path

--- create_tables.sql
+++ create_tables.sql
@@ -35,12 +35,13 @@ CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.dim_customer(
 
 -- COMMAND ----------
 
-CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.fact_sales(
-  sales_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  product_id BIGINT NOT NULL CONSTRAINT dim_product_fk FOREIGN KEY REFERENCES dim_product,
-  store_id BIGINT NOT NULL CONSTRAINT dim_store_fk FOREIGN KEY REFERENCES dim_store,
-  customer_id BIGINT NOT NULL CONSTRAINT dim_customer_fk FOREIGN KEY REFERENCES dim_customer,
-  price_sold DOUBLE,
-  units_sold INT,
-  dollar_cost DOUBLE
-);
+CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.fact_sales(  
+  sales_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,  
+  product_id BIGINT NOT NULL CONSTRAINT dim_product_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_product(product_id_column),  
+  store_id BIGINT NOT NULL CONSTRAINT dim_store_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_store(store_id_column),  
+  customer_id BIGINT NOT NULL CONSTRAINT dim_customer_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_customer(customer_id_column),  
+  price_sold DOUBLE,  
+  units_sold INT,  
+  dollar_cost DOUBLE  
+);  
+

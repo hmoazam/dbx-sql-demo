5782f1d5861ea6e15118c25456801c714db1d5d0
hmoazam <hmoazam>
2024-06-11 00:55:13 +0000

    fix pk/fk relationships

--- create_tables.sql
+++ create_tables.sql
@@ -32,9 +32,9 @@ CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.dim_customer(
 
 CREATE OR REPLACE TABLE uc_demos_hanna_moazam.default.fact_sales(  
   sales_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,  
-  product_id BIGINT NOT NULL CONSTRAINT dim_product_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_product(product_id_column),  
-  store_id BIGINT NOT NULL CONSTRAINT dim_store_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_store(store_id_column),  
-  customer_id BIGINT NOT NULL CONSTRAINT dim_customer_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_customer(customer_id_column),  
+  product_id BIGINT NOT NULL CONSTRAINT dim_product_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_product,  
+  store_id BIGINT NOT NULL CONSTRAINT dim_store_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_store,  
+  customer_id BIGINT NOT NULL CONSTRAINT dim_customer_fk FOREIGN KEY REFERENCES uc_demos_hanna_moazam.default.dim_customer,  
   price_sold DOUBLE,  
   units_sold INT,  
   dollar_cost DOUBLE  
